a21ce2971193852c00627c670b4e4721917d2f35
github username <github email address>
2019-06-15 14:43:49 -0500

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import {ProductConsumer} from '../context'
+import {Link} from 'react-router-dom'
+import {ButtonContainer} from './Button'
+
+export default class Details extends Component {
+        render(){
+    return (
+            <ProductConsumer>
+            {value => {
+                const {id,author,img,info,price,title,inCart} = value.detailProduct //assigned variable stores detailProduct for later use
+                return ( 
+                   <div className='container py-5'>
+                       {/* title */}
+                       <div className='row'>
+                           {/* <div className='col-10 mx-auto text-center text-slanted text-blue my-5'>
+                                <h1>
+                                {title}
+                                </h1>
+                           </div> */}
+                       </div>
+                       {/* end title */}
+                       {/* product info */}
+                       <div className='row'>
+                           <div className='col-10 mx-auto col-md-6 my-3'>
+                                <img src={img} className='img-fluid' alt='product'/>
+                           </div>
+                        {/* product text */}
+                           <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                                <h2>Title: {title}</h2>
+                                <h4 className='text-title text-uppercase text-muted mt-3 mb-2'>
+                                    Author: <span className='text-uppercase'>{author}</span>
+                                </h4>
+                                <h4 className='text-blue'>
+                                    <strong>
+                                        price: <span>$</span>{price}
+                                    </strong>
+                                </h4>
+                                <p className='text-capitalize font-weight-bold mt-3 mb-0'>
+                                    about: 
+                                </p>
+                                <p className='text-muted lead'>{info}</p>
+                                {/* buttons */}
+                                <div> 
+                                   <Link to='/'>
+                                       <ButtonContainer>Back to Books</ButtonContainer>
+                                   </Link>
+                                   {/* boolean value for button decides if 'add to cart' or 'in cart' displays */}
+                                   {/* ButtonContainer cart prop is passed to styled-component so color can change with status */}
+                                   {/* similiar to Context Api, must use a function for conditional rendering in Button.js to switch color to yellow if button is 'Add to Cart' */}
+                                   <ButtonContainer 
+                                   cart 
+                                   disabled={inCart? true : false} 
+                                   onClick={() => {
+                                       value.addToCart(id);
+                                       value.openModal(id);
+                                   }}
+                                   > 
+                                       {inCart?"inCart" : "add to cart"} 
+                                   </ButtonContainer> 
+                                </div>
+                           </div>
+                       </div>
+                   </div> 
+                )
+               }}
+           </ProductConsumer>
+        )
+        }
+    }
---
+++ src/data.js
@@ -0,0 +1,131 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const detailProduct = {
+  id: 1,
+  title: "Orthodoxy",
+  img: "img/product-1.png",
+  price: 10,
+  author: 'G.K. Chesterton',
+  info:"Orthodoxy (1908) is a book by G. K. Chesterton that has become a classic of Christian apologetics. Chesterton considered this book a companion to his other work, Heretics. In the book's preface Chesterton states the purpose is to attempt an explanation, not of whether the Christian faith can be believed, but of how he personally has come to believe it. In it, Chesterton presents an original view of Christian religion. He sees it as the answer to natural human needs, the answer to a riddle in his own words, and not simply as an arbitrary truth received from somewhere outside the boundaries of human experience.The book was written when Chesterton was an Anglican. He converted to Catholicism 14 years later. The title, Orthodoxy, is meant to avoid such sectarian questions.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Orthodoxy",
+    img: "img/product-1.png",
+    price: 10,
+    author: 'G.K. Chesterton',
+    info:"Orthodoxy (1908) is a book by G. K. Chesterton that has become a classic of Christian apologetics. Chesterton considered this book a companion to his other work, Heretics. In the book's preface Chesterton states the purpose is to attempt an explanation, not of whether the Christian faith can be believed, but of how he personally has come to believe it. In it, Chesterton presents an original view of Christian religion. He sees it as the answer to natural human needs, the answer to a riddle in his own words, and not simply as an arbitrary truth received from somewhere outside the boundaries of human experience.The book was written when Chesterton was an Anglican. He converted to Catholicism 14 years later. The title, Orthodoxy, is meant to avoid such sectarian questions.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "St. Thomas Aquinas: The Dumb Ox",
+    img: "img/product-2.png",
+    price: 16,
+    author: 'G.K. Chesterton',
+    info:"All of the usual caveats about Chesterton's writing apply here: he cannot resist a digression, he cannot resist an alliterative allusion, he cannot resist a pun. He is so full of life that he is constantly threatening to spin out of control. He is not a scholar, he is not writing a sober appraisal, he is probably not sure of most of the biographical details of his subject. In spite of these defects, the book is a triumph. Chesterton is the embodiment of A Man in Full; he is the polar opposite of C.S. Lewis' Men without Chests. He is so full of good sense, penetrating insight, sound moral judgement, and the joy of life that it is all spilling out in every direction. Anyone who has read his book of literary criticism on Dickens will understand these points: this is criticism in an old key; it is appreciative criticism; it is an encounter with a writer by an entire man, and not just by a theory. It is wonderfully refreshing. No one today writes in a similar vein. Chesterton brings all of his larger-than-life presence to bear on this account of the life (sort of) and thought of one of history's great minds. And on just what aspect of Thomas' thought does he focus? In one diabolically politically incorrect section near the end of the book he bellows out that on a map like the mind of Aquinas the mind of Luther was barely a speck, although he would hasten to add that his little book suffers the same ignoble comparison. There is a great deal to Thomas that he, of necessity, leaves out. But what he does include is very astutely chosen, for he understands the basic structure of Thomas' thought and emphasizes the essentials. Thus there is a chapter on Thomas' argument with the Manicheans and his affirmation of the goodness of the world. He treats with great aplomb Thomas' notion of being and its relation to God. He does great honor to Thomas' mode of argumentation, to his sober balance and fair treatment of opponents. He is appreciative of the devotional side of Thomas, which does not come through explicitly in his philosophical writings but is important for an understanding of the man. It must be granted that the book is as much about Chesterton as it is about Aquinas. Those wanting a more straight-forward treatment should seek out one of Josef Pieper's books on Aquinas. But if you have any adventurous spirit, by all means read this book. It is written by a man who loves and understands his subject in his very bones, and who brings his subject to life in a way that is most uncanny.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Heretics: Centennial Edition",
+    img: "img/product-3.png",
+    price: 8,
+    author: 'G.K. Chesterton',
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "The Everlasting Man",
+    img: "img/product-4.png",
+    price: 18,
+    author: 'G.K. Chesterton',
+    info:"An unabridged, unaltered edition to include: Prefatory Note – Introduction: The Plan of This Book – The Man in the Cave – Professors and Prehistoric Men – The Antiquity of Civilisation – God and Comparative Religion – Man and Mythologies – Demons and the Philosophers – The War of the Gods and Demons – The End of the World – The God in the Cave – The Riddles of the Gospel – The Strangest Story in the World – The Witness of the Heretics – The Escape from Paganism – The Five Deaths of the Faith – Conclusion: The Summary of This Book – Appendix I: On Prehistoric Man – Appendix II: On Authority and Accuracy –",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Father Brown (Complete Collection)",
+    img: "img/product-5.png",
+    price: 24,
+    author: 'G.K. Chesterton',
+    info:"Here you will find the complete Father Brown stories in the chronological order of their original publication.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "The Abolition of Man",
+    img: "img/product-6.png",
+    price: 17,
+    author: 'C.S. Lewis',
+    info:" In the classic The Abolition of Man, C.S. Lewis, the most important Christian writer of the 20th century, sets out to persuade his audience of the importance and relevance of universal values such as courage and honor in contemporary society. Both astonishing and prophetic, The Abolition of Man is one of the most debated of Lewis’s extraordinary works. National Review chose it as number seven on their 100 Best Nonfiction Books of the Twentieth Century.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "The Confessions (Oxford World's Classics)",
+    img: "img/product-7.png",
+    price: 30,
+    author: "Saint Augustine",
+    info:"In this new translation the brilliant and impassioned descriptions of Augustine's colourful early life are conveyed to the English reader with accuracy and art. Augustine tells of his wrestlings to master his sexual drive, his rare ascent from a humble Algerian farm to the edge of the corridors of high power at the imperial court of Milan, and his renunciation of secular ambition and marriage as he recovered the faith that his mother had taught him. It was in a Milan garden that Augustine finally achieved the act of will to Christian conversion, which he compared to a lazy man in bed finally deciding it is time to get up and face the day.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "City of God (Penguin Classics)",
+    img: "img/product-8.png",
+    price: 2,
+    author: "Saint Augustine",
+    info:"St Augustine, bishop of Hippo, was one of the central figures in the history of Christianity, and City of God is one of his greatest theological works. Written as an eloquent defence of the faith at a time when the Roman Empire was on the brink of collapse, it examines the ancient pagan religions of Rome, the arguments of the Greek philosophers and the revelations of the Bible. Pointing the way forward to a citizenship that transcends the best political experiences of the world and offers citizenship that will last for eternity, City of God is one of the most influential documents in the development of Christianity.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Joan of Arc (Ignatius Press)",
+    img: "img/product-9.png",
+    price: 11,
+    author: "Mark Twain",
+    info: "Very few people know that Mark Twain (Samuel Clemens) wrote a major work on Joan of Arc. Still fewer know that he considered it not only his most important but also his best work. He spent twelve years in research and many months in France doing archival work and then made several attempts until he felt he finally had the story he wanted to tell. He reached his conclusion about Joan's unique place in history only after studying in detail accounts written by both sides, the French and the English. Because of Mark Twain's antipathy to institutional religion, one might expect an anti-Catholic bias toward Joan or at least toward the bishops and theologians who condemned her. Instead one finds a remarkably accurate biography of the life and mission of Joan of Arc told by one of this country's greatest storytellers. The very fact that Mark Twain wrote this book and wrote it the way he did is a powerful testimony to the attractive power of the Catholic Church's saints. This is a book that really will inform and inspire.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Summa Theologica (All Complete & Unabridged)",
+    img: "img/product-10.png",
+    price: 11,
+    author: "Thomas Aquinas",
+    info: "This carefully crafted book: Summa Theologica (All Complete & Unabridged 3 Parts + Supplement & Appendix + interactive links and annotations) is formatted for your eReader with a functional and detailed table of contents. This book is the best-known work of Thomas Aquinas (c.1225–1274). Although unfinished, the Summa is one of the classics of the history of philosophy and one of the most influential works of Western literature. It is intended as an instructional guide for moderate theologians, and a compendium of all of the main theological teachings of the Catholic Church. It presents the reasoning for almost all points of Christian theology in the West. The Summa Theologica is divided into three parts, and each of these three parts contains numerous subdivisions. Part 1 deals primarily with God and comprises discussions of 119 questions concerning the existence and nature of God, the Creation, angels, the work of the six days of Creation, the essence and nature of man, and divine government. Part 2 deals with man and includes discussions of 303 questions concerning the purpose of man, habits, types of law, vices and virtues, prudence and justice, fortitude and temperance, graces, and the religious versus the secular life. Part 3 deals with Christ and comprises discussions of 90 questions concerning the Incarnation, the Sacraments, and the Resurrection. Some editions of the Summa Theologica include a Supplement comprising discussions of an additional 99 questions concerning a wide variety of loosely related issues such as excommunication, indulgences, confession, marriage, purgatory, and the relations of the saints toward the damned. Scholars believe that Rainaldo da Piperno, a friend of Aquinas, probably gathered the material in this supplement from a work that Aquinas had completed before he began working on the Summa Theologica.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
